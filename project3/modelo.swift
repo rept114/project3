614d47abb33296a3e950efecb8a37e977bb54077
Alumno ULSA <alumno@D523.local>
2022-11-02 08:54:49 -0700

    avance

---
+++ project3/modelo.swift
@@ -0,0 +1,12 @@
+//
+//  modelo.swift
+//  project3
+//
+//  Created by Alumno ULSA on 02/11/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import Foundation
+class modelo {
+    var mes: String
+}
