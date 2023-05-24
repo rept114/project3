a639e1ace5073810bd96120839046818156bcae8
Fatima Millanes <fatimamillanes@Fatimas-Mac-mini.local>
2023-05-24 01:22:12 -0700

    send

--- project3/AnimeController.swift
+++ project3/AnimeController.swift
@@ -8,16 +8,29 @@
 
 import Foundation
 import UIKit
+
 class AnimeController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var TvCapitulos: UITableViewCell!
-    @IBOutlet weak var TvDescripcion: UITableViewCell!
-    @IBOutlet weak var TvImagen: UITableViewCell!
+    @IBOutlet weak var TvAnimes: UITableView!
+    
     var capitulos: [Capitulo] = []
     var descripciones: [Descripcion] = []
     var animes: [Anime] = []
     var imagenes: [Imagen] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Registrar los identificadores de celda
+        TvAnimes.register(UINib(nibName: "CeldaAnime2Controller", bundle: nil), forCellReuseIdentifier: "anime2")
+        TvAnimes.register(UINib(nibName: "CeldaAnime3Controller", bundle: nil), forCellReuseIdentifier: "anime3")
+        
+        // Configurar la UITableView
+        TvAnimes.dataSource = self
+        TvAnimes.delegate = self
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return capitulos.count; descripciones.count; imagenes.count
+        return capitulos.count + descripciones.count + imagenes.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -25,13 +38,26 @@ class AnimeController: UIViewController, UITableViewDelegate, UITableViewDataSou
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celda2 = tableView.dequeueReusableCell(withIdentifier: "anime") as! CeldaAnime2Controller
-        celda2.episodio.text = capitulos[indexPath.row].episodio
-        celda2.duracion.text = capitulos[indexPath.row].duracion
-        let celda3 = tableView.dequeueReusableCell(withIdentifier: "anime") as! CeldaAnime3Controller
-        celda3.descripcion.text = descripciones[indexPath.row].descripcion
-        return celda2
+        if indexPath.row % 2 == 0 {
+            let celda2 = tableView.dequeueReusableCell(withIdentifier: "anime2", for: indexPath) as! CeldaAnime2Controller
+            
+            // Verificar si existe el capítulo correspondiente antes de acceder a él
+            if indexPath.row / 2 < capitulos.count {
+                celda2.episodio.text = capitulos[indexPath.row / 2].episodio
+                celda2.duracion.text = capitulos[indexPath.row / 2].duracion
+            }
+            
+            return celda2
+        } else {
+            let celda3 = tableView.dequeueReusableCell(withIdentifier: "anime3", for: indexPath) as! CeldaAnime3Controller
+            
+            // Verificar si existe la descripción correspondiente antes de acceder a ella
+            if indexPath.row / 2 < descripciones.count {
+                celda3.descripcion.text = descripciones[indexPath.row / 2].descripcion
+            }
+            
+            return celda3
+        }
     }
-    
-    
+
 }
--- project3/EscuelaController.swift
+++ project3/EscuelaController.swift
@@ -5,20 +5,17 @@
 //  Created by Alumno on 10/31/22.
 //  Copyright © 2022 Alumno. All rights reserved.
 //
-
 import UIKit
 
 class EscuelaController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var modelos : [Modelo] = []
-    var docentes:  [Docente] = []
-    var contactos: [Contactos] = []
-    var fechas : [Fecha] = []
+    var modelos: [Modelo] = []
     var capitulos: [Capitulo] = []
     var descripciones: [Descripcion] = []
     var animes: [Anime] = []
-    var imagenes: [Imagen] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         modelos.append(Modelo(segue: "anime", menu: "Naruto Shippuden", imagen: "descarga"))
@@ -27,53 +24,47 @@ class EscuelaController: UIViewController, UITableViewDelegate, UITableViewDataS
         modelos.append(Modelo(segue: "anime", menu: "Sword art online", imagen: "Kimetsu"))
         modelos.append(Modelo(segue: "anime", menu: "Assesination Classroom", imagen: "Kimetsu"))
         
-        animes[0].descripciones.append(Descripcion(descripcion: "Yuji Itadori decide pasar el tiempo con el Club de Ocultismo del instituto, pese a poseer unas habilidades atléticas extraordinarias. Con el tiempo, descubre que el mundo del oculto es real, y los miembros del club son atacados. Mientras, el misterioso Megumi Fushiguro está buscando un objeto maldito, y su búsqueda le lleva hasta Itadori…."))
-         
-        animes[0].capitulos.append(Capitulo(imagen: "", episodio: "Temporada: 1", duracion: "Episodios: 24"))
+        let anime = Anime() // Crear una instancia de Anime
+        anime.descripciones.append(Descripcion(descripcion: "Yuji Itadori decide pasar el tiempo con el Club de Ocultismo del instituto, pese a poseer unas habilidades atléticas extraordinarias. Con el tiempo, descubre que el mundo del oculto es real, y los miembros del club son atacados. Mientras, el misterioso Megumi Fushiguro está buscando un objeto maldito, y su búsqueda le lleva hasta Itadori…."))
+        anime.capitulos.append(Capitulo(imagen: "", episodio: "Temporada: 1", duracion: "Episodios: 24"))
         
-        animes[0].imagenes.append(Imagen(imagen: ""))
+        animes.append(anime) // Agregar el objeto Anime al array
         
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return modelos.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celda = tableView.dequeueReusableCell(withIdentifier: "celdaMenu") as! CeldaMenuController
         celda.lblMenu.text = modelos[indexPath.row].menu
         return celda
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //Se ejectua cuando se selecciona una celda del TableView
+        // Se ejecuta cuando se selecciona una celda del TableView
         performSegue(withIdentifier: modelos[indexPath.row].segue, sender: self)
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "anime" {
-        let destino = segue.destination as! HorarioAsistenciaController
-        }
-        if segue.identifier == "docente" {
-            let destino = segue.destination as! DocenteController
-            destino.docentes = docentes
-        }
-        if segue.identifier == "pagos" {
-            let destino = segue.destination as! CalendarioPagosController
-            destino.fechas = fechas
-        }
-        if segue.identifier == "contactoemergencia" {
-            let destino = segue.destination as! ContactoEmergenciaController
-            destino.contactos = contactos
+            let destino = segue.destination as! AnimeController
+            destino.capitulos = animes[0].capitulos
+            destino.descripciones = animes[0].descripciones
         }
     }
 }
-
